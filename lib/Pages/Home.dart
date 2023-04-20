b70298196d0bf6f875b91424153b4c5287b288da
7iilko <arabradui170@gmail.com>
2023-04-20 09:32:45 +0200

    chage color for CircleAvatar by used backgroundColor

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
           children: [
             CircleAvatar(
               radius: 50,
+              backgroundColor: Colors.white,
             )
           ],
         ),
